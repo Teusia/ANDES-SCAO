1bb3f6f77b1bee7be42a1f04d150229348ce8a93
Teusia <annelaure.cheffot@orange.fr>
2022-06-28 17:40:06 +0200

    (ALC) update radialProfile.py with binnerContrast and plotBinnedContrast

--- radialProfile.py
+++ radialProfile.py
@@ -77,76 +77,57 @@ def adjustCenter(img,dbg = False):
         plt.contour(fitted.reshape(xsize,xsize))
     return popt[1], popt[2]
 
-path2fits = '20220530-162324/'
-listFits = os.listdir(path2fits)
-listFits = [a for a in listFits if '.fits' in a]
-
-baseline = {1000:[[[25,85],[35,95]],[[10**-2,1.6*10**-3],[10**-2,1.6*10**-3]]],
-            1600:[[[25,85],[35,95]],[[10**-2,1.0*10**-3],[10**-2,1.0*10**-3]]],
-            2200:[[[25,85],[35,95]],[[10**-6,1.0*10**-6],[10**-6,1.0*10**-6]]]}
-goal = {1000: [[[25,85],[35,95]],[[10**-3,2.0*10**-4],[10**-3,2.0*10**-4]]],
-        1600: [[[25,85],[35,95]],[[10**-3,1.0*10**-4],[10**-3,1.0*10**-4]]],
-        2200:[[[25,85],[35,95]],[[10**-6,1.0*10**-6],[10**-6,1.0*10**-6]]]}
-ylimplot = [9*10**-6,1.5]
-
-
-
-plt.close('all')
-
-radialStop = 0.1 #arcsec
-binSize = 0.010#arcsec
-
-magnitude = ['diffraction limit',8,15,14]
-listCases = []
-
-for lf in listFits:
-
-    # with fits.open('part3-2000screen-1000nm.fits') as hdul1:
-    with fits.open(path2fits+lf) as hdul1:
-        # hdul1.info()
-        test = hdul1[0].data
-        wl = int(hdul1[0].header['WAVELEN']*10**9)
-        mag = hdul1[0].header['MAGNITUD']
-        listCases.append([wl,mag])
-        pix2arcsec = hdul1[0].header['PIXELSCL']
-        hdul1.close()
+def binnedContrast(psfData,binSize,pix2arcsec):
+    '''
+    this function takes the path to the file of the psf and file name, and the 
+    bin size in arcsec
     
-    #normalise the image
-    test *= 1/np.max(test)
-
-    imgSize = test.shape[0]
-
-    nbBin = np.floor(radialStop/binSize).astype(int)
-    binEdges = np.arange(binSize/2,radialStop,binSize)
+    parameters
+    ----------
+    psf: str
+        path and file name of the PSF.
+    binSize : float
+        size of the bin in mas
     
-    x = np.arange(-imgSize/2,imgSize/2)+0.5
+    output
+    ------
+        binCoord: array of float
+            position of the bin center including the center fo the PSF
+        binAvr: average of each bin distance
+        binMin: minimum of each bin ditance
+        binMax: maximum of each bin ditance
+        binStd: standard deviation of each bin
     
-    x*=pix2arcsec
-    X,Y = np.meshgrid(x,x)
-    R = np.sqrt(X**2+Y**2)
+    '''
+    #canculate the field of view
+    fov = psfData.shape[0]*pix2arcsec
+    #calculate the number of bin and their limites
+    nbBin = np.round(fov/2/binSize).astype(int)
+    binEdges = np.arange(binSize/2,fov/2,binSize)
     
-    #psf binning starts here
     #calculate the reference
     mask = R<=binSize/2
     ref = np.sum(test*mask)
     
-    nbBin = np.round(radialStop/binSize).astype(int)
+    #initialise variables to fill
     mmin = np.zeros(nbBin)
     mmax= np.zeros(nbBin)
     maverage = np.zeros(nbBin)
     mstd = np.zeros(nbBin)
     mmin[0] = mmax[0] = maverage[0] = ref
     
-    for rd in range(1,nbBin):
-        alpha = np.arctan(1/(4*rd))
-        nbPos = np.round(2*np.pi/alpha).astype(int)
+    for rd in range(1,nbBin):#for each bin
+        alpha = np.arctan(1/(2*rd))#find the displacement
+        nbPos = np.round(2*np.pi/alpha).astype(int)#the number of displacement
         curring = []
         for ap in range(nbPos):
-            
+            #build the mask to select the wanted pixel
             tmpX,tmpY = np.meshgrid(x-(rd*binSize*np.cos(alpha*ap)),
                                     x-(rd*binSize*np.sin(alpha*ap)))
             tmpR = np.sqrt(tmpX**2+tmpY**2)
             mask=tmpR<=binSize/2
+            
+            #accumulate the pixels sum
             curring.append(np.sum(test*mask))
         # plt.figure(10+rd)
         # plt.clf()
@@ -167,18 +148,115 @@ for lf in listFits:
         mmax[rd] = np.max(curring)
         maverage[rd] = np.mean(curring)
         mstd[rd] = np.std(curring)
-        
-        
     
-    xvar = np.arange(0,binEdges[-1],binSize)*10**3
+    mmin *=1/ref
+    mmax *=1/ref
+    maverage *=1/ref
+    mstd *= 1/ref
+    xvar = np.arange(0,binEdges[-1],binSize)
     
-    plt.figure(lf)
+    return xvar,mmin,mmax,maverage,mstd
+
+def plotBinnedContrast(binDat,wavelength,starType,saveFig = False,
+                       figName = None,xlimp = [],ylimp = []):
+    '''
+    plot the binned contrast for you
+
+    Parameters
+    ----------
+    binDat : tuple of numpy array
+        preferably directly the output of binnedContrast function
+    saveFig : bool (optional)
+        set to True if you want the figure saved automatically
+    figName : str (optionnal)
+        Set the name for the figure saved. This is not optionnal if you have 
+        set saveFig to True
+
+    Returns
+    -------
+    None.
+
+    '''
+    if saveFig and (figName is None):
+        print('give a name to the figure you want to save')
+        return 0
+    fig = plt.figure(figName)
     plt.clf()
-    plt.semilogy(xvar,mmin/ref,drawstyle = 'steps-mid')
-    plt.plot(xvar,mmax/ref,drawstyle = 'steps-mid')
-    toto = plt.errorbar(xvar, maverage/ref, yerr = mstd/ref,capsize=5
+    plt.semilogy(binDat[0]*10**3,binDat[1],drawstyle = 'steps-mid')
+    plt.plot(binDat[0]*10**3,binDat[2],drawstyle = 'steps-mid')
+    plt.errorbar(binDat[0]*10**3, binDat[3], yerr = binDat[4],capsize=5
                         ,drawstyle = 'steps-mid')
-    #psf binning finishes here
+    
+    plt.xlabel('distance from PSF center [mas]')
+    plt.ylabel('relative flux')
+    
+    plt.title('wl = {:.0f} nm, {}'.format(wavelength,starType))
+    custom_lines = [Line2D([0],[0],color='C0'),
+                    Line2D([0],[0],color='C1'),
+                    Line2D([0],[0],color='C2'),
+                    ]
+    plt.legend(custom_lines,['min','max','average+std',
+                             ],
+               handletextpad=0.2,
+               labelspacing = 0.2)
+    if ylimp:
+        plt.ylim(ylimp)
+    if xlimp:
+        plt.xlabel(xlimp)
+    plt.grid()
+    plt.tight_layout()
+    
+
+#%%
+path2fits = '20220530-162324/'
+listFits = os.listdir(path2fits)
+listFits = [a for a in listFits if '.fits' in a]
+
+baseline = {1000:[[[25,85],[35,95]],[[10**-2,1.6*10**-3],[10**-2,1.6*10**-3]]],
+            1600:[[[25,85],[35,95]],[[10**-2,1.0*10**-3],[10**-2,1.0*10**-3]]],
+            2200:[[[25,85],[35,95]],[[10**-6,1.0*10**-6],[10**-6,1.0*10**-6]]]}
+goal = {1000: [[[25,85],[35,95]],[[10**-3,2.0*10**-4],[10**-3,2.0*10**-4]]],
+        1600: [[[25,85],[35,95]],[[10**-3,1.0*10**-4],[10**-3,1.0*10**-4]]],
+        2200:[[[25,85],[35,95]],[[10**-6,1.0*10**-6],[10**-6,1.0*10**-6]]]}
+ylimplot = [9*10**-6,1.5]
+
+
+
+plt.close('all')
+
+radialStop = 0.25 #arcsec
+binSize = 0.010#arcsec
+
+magnitude = ['diffraction limit',8,15,14]
+listCases = []
+
+for lf in listFits:
+
+    with fits.open(path2fits+lf) as hdul1:
+        # hdul1.info()
+        test = hdul1[0].data
+        wl = int(hdul1[0].header['WAVELEN']*10**9)
+        mag = hdul1[0].header['MAGNITUD']
+        listCases.append([wl,mag])
+        pix2arcsec = hdul1[0].header['PIXELSCL']
+        hdul1.close()
+    
+    #normalise the image
+    test *= 1/np.max(test)
+
+    imgSize = test.shape[0]
+
+    nbBin = np.floor(radialStop/binSize).astype(int)
+    # binEdges = np.arange(binSize/2,radialStop,binSize)
+    
+    x = np.arange(-imgSize/2,imgSize/2)+0.5
+    
+    x*=pix2arcsec
+    X,Y = np.meshgrid(x,x)
+    R = np.sqrt(X**2+Y**2)
+    
+    binData = binnedContrast(test, binSize,pix2arcsec)
+    plotBinnedContrast(binData,wl,mag,figName = lf)
     
     #adjust the psf center
     # centering = adjustCenter(test)
@@ -227,30 +305,26 @@ for i in wluni:
     plt.tight_layout()
 
 #binned psf profiles details of everything
-for lf in range(len(listFits)):
+# for lf in range(len(listFits)):
     
-    plt.figure(listFits[lf])
-    plt.plot(baseline[listCases[lf][0]][0],baseline[listCases[lf][0]][1],'r',linewidth = 3)
-    plt.plot(goal[listCases[lf][0]][0],goal[listCases[lf][0]][1],'m',linewidth = 3)
-    plt.xlabel('distance from PSF center [mas]')
-    plt.ylabel('relative flux')
+#     plt.figure(listFits[lf])
+#     plt.plot(baseline[listCases[lf][0]][0],baseline[listCases[lf][0]][1],'r',linewidth = 3)
+#     plt.plot(goal[listCases[lf][0]][0],goal[listCases[lf][0]][1],'m',linewidth = 3)
+#     plt.xlabel('distance from PSF center [mas]')
+#     plt.ylabel('relative flux')
     
-    plt.title('wl = {:.0f} nm, {}'.format(listCases[lf][0],listCases[lf][1]))
-    custom_lines = [Line2D([0],[0],color='C0'),
-                    Line2D([0],[0],color='C1'),
-                    Line2D([0],[0],color='C2'),
-                    Line2D([0],[0],color = 'r'),
-                    Line2D([0],[0],color = 'm')
-                    ]
-    plt.legend(custom_lines,['min','max','average+std',
-                                'TRS Baseline',
-                                'GOAL',
-                             ],
-               handletextpad=0.2,
-               labelspacing = 0.2)
-    plt.ylim(ylimplot)
-    plt.grid()
-    plt.tight_layout()
+#     plt.title('wl = {:.0f} nm, {}'.format(listCases[lf][0],listCases[lf][1]))
+#     custom_lines = [Line2D([0],[0],color='C0'),
+#                     Line2D([0],[0],color='C1'),
+#                     Line2D([0],[0],color='C2'),
+#                     ]
+#     plt.legend(custom_lines,['min','max','average+std',
+#                              ],
+#                handletextpad=0.2,
+#                labelspacing = 0.2)
+#     plt.ylim(ylimplot)
+#     plt.grid()
+    # plt.tight_layout()
 
 # plt.semilogy([90,90], [5*10**-5,1],'k--')
 # plt.semilogy([30,30], [5*10**-5,1],'k--')
